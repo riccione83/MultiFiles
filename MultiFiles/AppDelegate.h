9f3ee274b7b866645e57b58cd596368b9cecda6a
Riccardo Rizzo <r.riki@tiscali.it>
2015-05-12 11:04:29 +0200

    Initial Commit

---
+++ MultiFiles/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultiFiles
+//
+//  Created by Riccardo Rizzo on 12/05/15.
+//  Copyright (c) 2015 Riccardo Rizzo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MultiFiles/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MultiFiles
+//
+//  Created by Riccardo Rizzo on 12/05/15.
+//  Copyright (c) 2015 Riccardo Rizzo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
